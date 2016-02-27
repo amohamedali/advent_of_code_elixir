80dab1821e0ffde18e4a6d2e94e3d0e4298137f6
Babman <b.feuillardhatton@hotmail.fr>
2016-02-27 10:46:02 +0100

    refactor for readability

--- ex1/part_2.exs
+++ ex1/part_2.exs
@@ -6,15 +6,15 @@ defmodule Santa do
 	end
 
 	def help_santa body do
-		String.to_char_list(body)
+		body
 		|> count(0, 0)
 	end
 
 	def count(_body, -1, nth_guy), do: nth_guy
 
-	def count([40 | body], acc, nth_guy), do: count(body, acc + 1, nth_guy + 1)
+	def count("(" <> body, acc, nth_guy), do: count(body, acc + 1, nth_guy + 1)
 
-	def count([41 | body], acc, nth_guy), do: count(body, acc - 1, nth_guy + 1)
+	def count(")" <> body, acc, nth_guy), do: count(body, acc - 1, nth_guy + 1)
 
 	def count([], _acc, _nth_guy), do: "never reached"
 
--- ex2/part_2.exs
+++ ex2/part_2.exs
@@ -1,24 +1,26 @@
 defmodule Santa do
+
 	def help input_file do
 		File.read!(input_file)
 		|> help_santa
-		|> IO.puts
 	end
 
 	def help_santa body do
-		String.split(body, "\n")
-		|> Enum.reduce(0, &(get_box_size(&1) + &2))
+		get_path(body, [[0, 0]])
+		|> Enum.uniq
+		|> Enum.count
+		|> IO.puts
 	end
 
+	def get_path("<" <> body, [[x, y] | path]), do: get_path(body, [[x - 1, y], [x, y] | path])
 
-	def get_box_size box_dimensions do
-		[l,w,h] = String.split(box_dimensions, "x")
-		{li, ""} = Integer.parse l
-		{wi, ""} = Integer.parse w
-		{hi, ""} = Integer.parse h
-		min = Enum.min([li + wi, hi + wi, li + hi])
-		li * wi * hi + 2 * min
-	end
+	def get_path(">" <> body, [[x, y] | path]), do: get_path(body, [[x + 1, y], [x, y] | path])
+
+	def get_path("v" <> body, [[x, y] | path]), do: get_path(body, [[x, y - 1], [x, y] | path])
+
+	def get_path("^" <> body, [[x, y] | path]), do: get_path(body, [[x, y + 1], [x, y] | path])
+
+	def get_path("", path), do: path
 end
 
 Santa.help "part_1.input"
